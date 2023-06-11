05c861ef6783d8552738d40192c256742223ae06
Rora Alem <rora.goitom@gmail.com>
2023-06-11 11:23:07 -0400

    Initial commit

---
+++ spinner1.js
@@ -0,0 +1,18 @@
+// one full animation of spinner
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+   // Need to escape the backslash since it's a special character.
+   process.stdout.write('\r\\   ');
+}, 700);
+
